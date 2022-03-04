f1c2be107d01f8093b087d079f85cad32b7435dc
lebrancconvas <lebrancconvas@gmail.com>
2022-03-04 12:25:33 +0700

    :bulb: Delete Comment.

--- TicTacToe_Lab/TicTacToe/TicTacToe/TicTacToe.cs
+++ TicTacToe_Lab/TicTacToe/TicTacToe/TicTacToe.cs
@@ -6,7 +6,6 @@ namespace TicTacToe
 {
     public class TicTacToe : Game
     {
-        // Test Visual Studio 2019 Commit again. 
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         Texture2D _line, _circle, _cross;
