f5086dedda1fc947522c2f35919600142ca19a4b
lebrancconvas <lebrancconvas@gmail.com>
2022-03-16 23:56:24 +0700

    :tada: Init The Snake.

---
+++ The-Snake-Lab/The-Snake-Lab/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace The_Snake_Lab
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new TheSnake())
+                game.Run();
+        }
+    }
+}
